d07383df63fb505ff0b7b9537d79aabb180308ee
Rajesh18rj <rajeshkumar18rj@gmail.com>
2024-11-22 18:30:21 +0530

    -

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -4,24 +4,40 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use Livewire\Attributes\Rule;
+use Livewire\WithPagination;
 
 class Clicker extends Component
 {
+    use WithPagination;
+
+    #[Rule('required|min:2|max:30')]
     public $name;
+
+    #[Rule('required|email|unique:users')]
     public $email;
+
+    #[Rule('required|min:5')]
     public $password;
 
     public function createNewUser(){
+
+        $validated = $this->validate();
+
         User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password'=> $this->password
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password'=> $validated['password']
         ]);
+
+        $this->reset(['name', 'email', 'password']);
+
+        request()->session()->flash('success', 'User Created Successfully!');
     }
 
     public function render()
     {
-        $users= User::all();
+        $users= User::paginate(5);
 
         return view('livewire.clicker',[
             'users'=>$users
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,18 +1,36 @@
 <div>
-    <form wire:submit="createNewUser" action="">
-        <input wire:model="name" type="text" placeholder="name">
-        <input wire:model="email" type="email" placeholder="email">
-        <input wire:model="password" type="password" placeholder="password">
+    @if(session('success'))
+        <span class="w-100 py-3 bg-green-300 rounded">{{session('success')}}</span>
+    @endif
 
-        <button>Create</button>
+    <form wire:submit="createNewUser" class="p-4 space-y-4 bg-gray-100 rounded">
+
+        <input class="block w-80 p-2 border rounded" wire:model="name" type="text" placeholder="Name">
+        @error('name')
+            <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <input class="block w-80 p-2 border rounded" wire:model="email" type="email" placeholder="Email">
+        @error('email')
+        <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <input class="block w-80 p-2 border rounded" wire:model="password" type="password" placeholder="Password">
+        @error('password')
+        <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <button class="w-40 px-3 p-2 text-white bg-blue-500 rounded hover:bg-blue-600">Create</button>
+
     </form>
+
 
     <hr>
 
     @foreach ($users as $user)
         <p>{{$user->name}}</p>
-        <p>{{$user->email}}</p>
-        <p>{{$user->password}}</p>
     @endforeach
+
+     {{ $users->links(vendor.livewire.test) }}
 
 </div>
