1f8152700e58bc1d5fd90c47759b6aae9c3a4aa1
Rajesh18rj <rajeshkumar18rj@gmail.com>
2024-11-28 19:39:57 +0530

    #CRUD

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,17 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
     public $name;
-
+    public $search;
     public function create(){
+//        dd('test');
+        //validate
+        //create the to-do
+        //clear the input
+        //send flash message
 
+        $validated = $this->validate([
+            'name'=>'required|min:3|max:50',
+        ]);
+
+        Todo::create($validated);
+
+        $this->reset('name');
+
+        session()->flash('success', 'Todo created successfully.');
+
+    }
+
+    public function delete(Todo $todo){
+        $todo->delete();
     }
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()
+                ->where('name', 'like', "%{$this->search}%")
+                ->paginate(3)
+        ]);
     }
 }
--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    protected $guarded=[];
+    protected $fillable=['name'];
+
 }
